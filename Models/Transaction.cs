6d4254ef87112df14a081c66af7c2115e76f2a79
Rohan Hangal <rohanhangal@gmail.com>
2023-07-28 22:52:25 -0700

    first commit

---
+++ Models/Transaction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTransaction.Models
+{
+	public class Transaction
+	{
+		[Key]
+		public int TransactionId{ get; set;}
+
+		[Required(ErrorMessage ="This field is required.")]
+		[DisplayName("Account Number")]
+		[Column(TypeName ="varchar(12)")]
+		[MaxLength(12,ErrorMessage ="Maximum 12 characters only.")]
+		public String AccountNumber{ get; set;}
+
+        [Required(ErrorMessage = "This field is required.")]
+        [DisplayName("Beneficiary Name")]
+		[Column(TypeName ="varchar(100)")]
+		public String BeneficiaryName{ get; set;}
+
+        [Required(ErrorMessage = "This field is required.")]
+        [DisplayName("Bank Name")]
+        [Column(TypeName ="varchar(100)")]
+		public String BankName{ get; set;}
+
+        [Required(ErrorMessage = "This field is required.")]
+        [DisplayName("SWIFT Code")]
+        [Column(TypeName ="varchar(11)")]
+        [MaxLength(11, ErrorMessage = "Maximum 11 characters only.")]
+        public String SWIFTCode{ get; set;}
+
+        [Required(ErrorMessage = "This field is required.")]
+        public int Amount{ get; set;}
+
+		[DisplayFormat(DataFormatString ="{0:MMM-dd-yy}")]
+		public DateTime Date{ get; set;}
+	}
+}
+
